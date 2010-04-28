3f27190316f49da6837b13cbf7284978aea96a61
priscillakmv <priscillakmv@15ea17dd-a405-5fa5-e9b7-e02a8565f0a8>
2010-04-28 14:28:50 +0000

    Relacionamento entre as tabelas

--- src/schemagenerator/ListTable.java
+++ src/schemagenerator/ListTable.java
@@ -3,10 +3,11 @@
  * and open the template in the editor.
  */
 
-package schemagenerator;
 
+package schemagenerator;
 import java.util.ArrayList;
 import java.util.Iterator;
+import sun.applet.Main;
 
 /**
  *
@@ -15,27 +16,104 @@ import java.util.Iterator;
 public class ListTable {
 
 
-    private ArrayList<TableRelation> listTables;
-    
+    private ArrayList<TableRelation> listTables = new ArrayList<TableRelation>() ;
+
     public ArrayList getTables(){
         return this.listTables;
     }
+
+    /**
+     *
+     * @param tableFrom
+     * @param tableTo
+     */
       public void setRelation(String tableFrom, String tableTo){
-          
-          boolean found = false;
-          for (Iterator<TableRelation> it = listTables.iterator(); it.hasNext();) {
-              TableRelation tableRelation = it.next();
+          int indexFrom =0;
+          int indexTo = 0;
 
-              if (tableRelation.getNameOfTable().equals(tableFrom)) {
-                  found = true;
-              }
-          }
+          if (getTableIndex(tableFrom)== -1 ) {
+           System.out.println("FROM N EXISTE");
 
-          if (found = false) {
               TableRelation table = new TableRelation(tableFrom);
               this.listTables.add(table);
-          } 
+          }
 
-    }   
+          if (getTableIndex(tableTo)== -1 ) {
+
+              TableRelation table = new TableRelation(tableTo);
+              this.listTables.add(table);
+          }
+           indexFrom = getTableIndex(tableFrom);
+           indexTo = getTableIndex(tableTo);
+           // this.listTables.get(indexFrom).setRelation(listTables.get(indexTo));
+           getTable(indexFrom).setRelation(getTable(indexTo));
+
+    }
+
+
+      public int getTableIndex(String nameTable){
+
+          int index = -1;
+
+          System.out.println("pega o index");
+
+          for (Iterator<TableRelation> it = listTables.iterator(); it.hasNext();) {
+              TableRelation tableRelation = it.next();
+              System.out.println("não tá vazio");
+             if (tableRelation.getNameOfTable().equals(nameTable)) {
+                 index = listTables.indexOf(tableRelation);
+                  System.out.println("entrou no if  " + index + " " + nameTable);
+
+              }
+
+
+          }
+
+          System.out.println(index);
+          return index;
+      }
+
+
+       public TableRelation getTable(int index){
+           return this.listTables.get(index);
+
+      }
+        public ArrayList getListOfTables(){
+           return this.listTables;
+
+      }
+
+        public static void main(String[] args) {
+        ListTable lista = new ListTable();
+        System.out.println("ajsdjdjudfi");
+        lista.setRelation("a", "b");
+        System.out.println("KKKKKKKKKKKKKKKKKKKKKKK");
+        lista.setRelation("a", "c");
+       lista.setRelation("d", "c");
+        System.out.println("JIDSJSIODJSIODJASIOD");
+
+           for (Iterator<TableRelation> it = lista.getListOfTables().iterator(); it.hasNext();) {
+              TableRelation tableRelation = it.next();
+             System.out.println(tableRelation.getNameOfTable() + "   resultado ");
+
+
+          }
+
+
+
+            for (int i = 0;  i < 2; i++) {
+
+                TableRelation tableR = lista.getTable(0);
+                ArrayList<TableRelation> array = tableR.getRelations();
+                TableRelation tableRelation = array.get(i);
+
+                System.out.println( tableRelation.getNameOfTable() + "resultado");
+
+            }
+
+
+        }
+
+
 
 }
